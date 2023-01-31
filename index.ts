326d8980ccbbd80d3f8719dcd20ab006c2836b39
Juhani Pelli <juhani.pelli@gmail.com>
2023-01-31 20:06:05 +0200

    Add dumb line processing for stdin stream

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+const readline = require('node:readline');
+
+const input = readline.createInterface({
+  input: process.stdin,
+});
+
+let lineNumber = 0;
+input.on('line', (line: string) => {
+  lineNumber++
+  const lineId = lineNumber.toString().padStart(3, '0')
+  process.stdout.write(`${lineId} ${line}\n`)
+})
