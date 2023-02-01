44297a04d089e821c01d33f73ce7d205c419d47b
Juhani Pelli <juhani.pelli@gmail.com>
2023-02-01 12:43:29 +0200

    Coerce input to type using zod

--- lib/listener.ts
+++ lib/listener.ts
@@ -1,37 +1,59 @@
+import { z } from 'zod'
 
-export type RuuvitagMeasurementData = {
-  acceleration_x: string
-  acceleration_y: string
-  acceleration_z: string
-  battery_potential: string
-  humidity: string
-  measurement_sequence_number: string
-  movement_counter: string
-  pressure: string
-  temperature: string
-  tx_power: string
-}
+export const ZRuuvitagMeasurementData = z.object({
+  acceleration_x: z.number(),
+  acceleration_y: z.number(),
+  acceleration_z: z.number(),
+  battery_potential: z.number(),
+  humidity: z.number(),
+  measurement_sequence_number: z.number(),
+  movement_counter: z.number(),
+  pressure: z.number(),
+  temperature: z.number(),
+  tx_power: z.number(),
+})
 
-export type RuuvitagMeasurement = {
-  id: string
-  mac: string
-  data: RuuvitagMeasurementData
-  time: number
-}
+export const ZRuuvitagMeasurement = z.object({
+  id: z.string().length(12),
+  mac: z.string().length(17),
+  data: ZRuuvitagMeasurementData,
+  time: z.number(),
+})
+
+const ZRuuvitagMeasurementDataFromParsedInput = z.object({
+  acceleration_x: z.coerce.number(),
+  acceleration_y: z.coerce.number(),
+  acceleration_z: z.coerce.number(),
+  battery_potential: z.coerce.number(),
+  humidity: z.coerce.number(),
+  measurement_sequence_number: z.coerce.number(),
+  movement_counter: z.coerce.number(),
+  pressure: z.coerce.number(),
+  temperature: z.coerce.number(),
+  tx_power: z.coerce.number(),
+})
+
+const ZRuuvitagIdentifiersFromParsedInput = z.object({
+  mac: z.string()
+})
+
+export type RuuvitagMeasurementData = z.infer<typeof ZRuuvitagMeasurementData>
+
+export type RuuvitagMeasurement = z.infer<typeof ZRuuvitagMeasurement>
 
 
-function parseKeyValueCsv<TData extends Record<string, string>>(line: string): Record<keyof TData, string> {
+function parseKeyValueCsv<TData extends Record<string, unknown>>(line: string): Record<keyof TData, string> {
   const pairs = line.split(',')
   return Object.fromEntries(pairs.map(pair => pair.split('=')))
 }
 
 export function parseLineFromRuuvitagListener(line: string): RuuvitagMeasurement {
   const [identifiers, data, time] = line.split(' ')
-  const {mac} = parseKeyValueCsv<{mac: string}>(identifiers)
+  const {mac} = ZRuuvitagIdentifiersFromParsedInput.parse(parseKeyValueCsv<{mac: string}>(identifiers))
   return {
     id: mac.toLowerCase().replace(/:/g, ''),
     mac,
-    data: parseKeyValueCsv<RuuvitagMeasurementData>(data),
-    time: Number(time)
+    data: ZRuuvitagMeasurementDataFromParsedInput.parse(parseKeyValueCsv<RuuvitagMeasurementData>(data)),
+    time: z.coerce.number().parse(time)
   }
 }
