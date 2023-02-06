1b17d0b02375c5b0d83dd46a8692e66e3eda5459
Juhani Pelli <juhani.pelli@gmail.com>
2023-02-06 17:57:24 +0200

    Add influxdb library package

---
+++ packages/common-influxdb/lib/write-api.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod'
+import { InfluxDB } from '@influxdata/influxdb-client'
+
+const ZEnv = z.object({
+  INFLUX_URL: z.string(),
+  INFLUX_TOKEN: z.string(),
+  INFLUX_ORG: z.string(),
+  INFLUX_BUCKET: z.string(),
+})
+
+const env = ZEnv.parse(process.env)
+
+export function createWriteApi() {
+  const db = new InfluxDB({ url: env.INFLUX_URL, token: env.INFLUX_TOKEN })
+  return db.getWriteApi(env.INFLUX_ORG, env.INFLUX_BUCKET)
+}
