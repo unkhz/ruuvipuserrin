41b8f69c477c03bbfd47d6cd2fbaa014cd096b47
Juhani Pelli <juhani.pelli@gmail.com>
2023-02-14 12:13:00 +0200

    Report coverage for jest packages

--- packages/common-influxdb/jest.config.ts
+++ packages/common-influxdb/jest.config.ts
@@ -3,6 +3,8 @@ import type { JestConfigWithTsJest } from 'ts-jest'
 const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  coverageReporters: ['text'],
 }
 
 export default config
