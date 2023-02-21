80fc37efab6aeee0174eb2c0e040435c480b4e15
Juhani Pelli <juhani.pelli@gmail.com>
2023-02-21 15:06:53 +0200

    Use daisyUI for CSS-only tailwind components

--- packages/configurator/tailwind.config.js
+++ packages/configurator/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,jsx,ts,tsx}', '../../node_modules/flowbite/**/*.{js,jsx,ts,tsx}'],
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'media', // Use media queries for dark mode
   theme: {
     extend: {},
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [require('daisyui')],
 }
