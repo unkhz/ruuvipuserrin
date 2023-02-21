ee147cde1865bfeaf7e444f0ae8b963bb1eea26c
Juhani Pelli <juhani.pelli@gmail.com>
2023-02-21 15:07:24 +0200

    Implement config edit with mock data & schema

--- packages/configurator/app/routes/index.tsx
+++ packages/configurator/app/routes/index.tsx
@@ -1,18 +1,32 @@
-import type { LinksFunction } from '@remix-run/cloudflare'
+import type { ActionArgs, LinksFunction } from '@remix-run/cloudflare'
+import { redirect } from '@remix-run/cloudflare'
 import { json } from '@remix-run/cloudflare'
-import { useLoaderData, useSearchParams } from '@remix-run/react'
+import { Form, useLoaderData, useSearchParams } from '@remix-run/react'
+import type { Item } from '~/utils/db.server'
+import { schema } from '~/utils/db.server'
 import db from '~/utils/db.server'
 
 import tailwindCss from '~/styles/tailwind.css'
 import indexCss from '~/styles/index.css'
-import { Modal, Navbar, Table } from 'flowbite-react'
 
 export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: tailwindCss },
   { rel: 'stylesheet', href: indexCss },
   { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Anton|Barlow' },
-  { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css' },
 ]
+
+export const action = async ({ request }: ActionArgs) => {
+  const form = await request.formData()
+  const id = form.get('id')
+  const name = form.get('name')
+  const location = form.get('location')
+  console.log('dat', { id, name, location })
+  if (typeof id !== 'string' || typeof name !== 'string' || typeof location !== 'string') {
+    throw new Error('Invalid form data')
+  }
+  await db.write(id, { name, location })
+  return redirect('/')
+}
 
 export const loader = async () => {
   return json({
@@ -20,59 +34,88 @@ export const loader = async () => {
   })
 }
 
-function EditModal() {
+function SourceEditModal({ item }: { item: Item }) {
   return (
-    <Modal show={false}>
-      <Modal.Header>Terms of Service</Modal.Header>
-      <Modal.Body>
-        <div className="space-y-6">
-          <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-            With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
-            companies around the world are updating their terms of service agreements to comply.
-          </p>
-          <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-            The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant
-            to ensure a common set of data rights in the European Union. It requires organizations to notify users as
-            soon as possible of high-risk data breaches that could personally affect them.
-          </p>
-        </div>
-      </Modal.Body>
-    </Modal>
+    <div className="modal modal-open">
+      <div className="modal-box">
+        <Form className="form-control" method="post">
+          <h3 className="text-lg mx-2">Edit source: {item.id}</h3>
+          {schema.map(({ name, autofocus, editable, description }) => {
+            const htmlId = `edit-item-${item.id}-${name}`
+            return (
+              <fieldset key={name} className="mx-1">
+                <label className="label" htmlFor={htmlId}>
+                  <span className="label-text">{description}</span>
+                </label>
+                <input
+                  className="input input-bordered"
+                  name={name}
+                  id={htmlId}
+                  type="text"
+                  autoFocus={autofocus}
+                  defaultValue={item[name]}
+                  hidden={!editable}
+                />
+              </fieldset>
+            )
+          })}
+          <div className="modal-action">
+            <a className="btn" href="/">
+              Cancel
+            </a>
+            <input type="submit" className="btn" value="Submit" />
+          </div>
+        </Form>
+      </div>
+    </div>
+  )
+}
+
+function SourcesTable() {
+  const data = useLoaderData<typeof loader>()
+  const [searchParams] = useSearchParams()
+  const editItemId = searchParams.get('edit')
+  const editItem = data.items.find((item) => item.id === editItemId)
+  return (
+    <>
+      <table className="table w-full">
+        <thead>
+          <th>ID</th>
+          <th>Listener ID</th>
+          <th>Name</th>
+          <th>Location</th>
+          <th></th>
+        </thead>
+        <tbody>
+          {data.items.map(({ id, listener_id = '<none>', name = '<none>', location = '<none>' }) => (
+            <tr key={id}>
+              <td>{id}</td>
+              <td>{listener_id}</td>
+              <td>{name}</td>
+              <td>{location}</td>
+              <td>
+                <a className="btn-link" href={`?edit=${id}`}>
+                  Edit
+                </a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {editItem ? <SourceEditModal item={editItem} /> : null}
+    </>
   )
 }
 
 export default function ConfigRoute() {
-  const data = useLoaderData<typeof loader>()
-  const [searchParams] = useSearchParams()
-  const editItemId = searchParams.get('edit')
   return (
     <main>
-      <Navbar className="w-full px-2 py-4 text-xl sm:text-5xl">Ruuvipuserrin Tag Configurator</Navbar>
+      <nav className="navbar w-full bg-zinc-100 dark:bg-zinc-900 px-2 py-4 text-xl sm:text-5xl">
+        Ruuvipuserrin Tag Configurator
+      </nav>
       <div className="flex justify-center sm:px-8 sm:py-16">
         <div className="container flex-none mx-auto max-w-4xl">
-          <Table>
-            <Table.Head className="text-lef">
-              <Table.HeadCell>ID</Table.HeadCell>
-              <Table.HeadCell>Listener ID</Table.HeadCell>
-              <Table.HeadCell>Name</Table.HeadCell>
-              <Table.HeadCell>Location</Table.HeadCell>
-              <Table.HeadCell></Table.HeadCell>
-            </Table.Head>
-            <Table.Body>
-              {data.items.map(({ id, listener_id = '<none>', name = '<none>', location = '<none>' }) => (
-                <Table.Row key={id}>
-                  <Table.Cell>{id}</Table.Cell>
-                  <Table.Cell>{listener_id}</Table.Cell>
-                  <Table.Cell>{name}</Table.Cell>
-                  <Table.Cell>{location}</Table.Cell>
-                  <Table.Cell>
-                    <a href={`?edit=${id}`}>Edit</a>
-                  </Table.Cell>
-                </Table.Row>
-              ))}
-              {editItemId ? <EditModal /> : null}
-            </Table.Body>
-          </Table>
+          <SourcesTable />
         </div>
       </div>
     </main>
--- packages/configurator/app/utils/db.server.ts
+++ packages/configurator/app/utils/db.server.ts
@@ -1,17 +1,37 @@
-type Item = {
+export type Item = {
   id: string
   listener_id: string
   name?: string
   location?: string
 }
 
-const stuff: Item[] = [
-  { id: 'a', listener_id: 'irue983', name: 'stuff', location: 'drawer' },
-  { id: 'b', listener_id: 'irue983', name: 'exist', location: 'abstract' },
-  { id: 'c', listener_id: 'e83kjsd', name: 'here', location: 'place' },
+export type FormInput = { editable: boolean; autofocus: boolean; name: keyof Item; description: string }
+
+export const schema: FormInput[] = [
+  { editable: false, autofocus: false, name: 'id', description: 'Source Device ID' },
+  { editable: false, autofocus: false, name: 'listener_id', description: 'Listener ID' },
+  { editable: true, autofocus: true, name: 'name', description: 'Source Device Name' },
+  { editable: true, autofocus: false, name: 'location', description: 'Source Device Location' },
 ]
 
+const stuff: Map<string, Item> = new Map([
+  ['a', { id: 'a', listener_id: 'irue983', name: 'stuff', location: 'drawer' }],
+  ['b', { id: 'b', listener_id: 'irue983', name: 'exist', location: 'abstract' }],
+  ['c', { id: 'c', listener_id: 'e83kjsd', name: 'here', location: 'place' }],
+])
+
 export default {
-  read: () => stuff,
-  write: (thing: Item) => stuff.push(thing),
+  read: () => Array.from(stuff.values()),
+  write: (id: string, input: Pick<Item, 'name' | 'location'>) => {
+    const originalItem = stuff.get(id)
+    if (!originalItem) {
+      throw new Error(`Item with id ${id} not found`)
+    }
+    const modifiedItem: Item = {
+      ...originalItem,
+      name: input.name ?? originalItem.name,
+      location: input.location ?? originalItem.location,
+    }
+    stuff.set(id, modifiedItem)
+  },
 }
