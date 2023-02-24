721f1c0ae337b81dc500a3d9de694ca2cd0146ed
Juhani Pelli <juhani.pelli@gmail.com>
2023-02-24 15:27:47 +0200

    Cleanup

--- packages/common-archive-client/migrations/000-use-timescaledb.ts
+++
@@ -1,9 +0,0 @@
-import { Kysely, sql } from 'kysely'
-
-export async function up(db: Kysely<any>) {
-  sql`CREATE EXTENSION IF NOT EXISTS timescaledb;`.execute(db)
-}
-
-export async function down(db: Kysely<any>): Promise<void> {
-  sql`DROP EXTENSION timescaledb;`.execute(db)
-}
