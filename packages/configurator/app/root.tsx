8caf4f68993bcf528538779cc91942b78aa1c1cf
Juhani Pelli <juhani.pelli@gmail.com>
2023-03-07 15:01:18 +0200

    Add quick styling to configurator errors

--- packages/configurator/app/root.tsx
+++ packages/configurator/app/root.tsx
@@ -1,4 +1,4 @@
-import { Links, LiveReload, Outlet } from '@remix-run/react'
+import { Links, LiveReload, Outlet, Scripts } from '@remix-run/react'
 import type { LinksFunction } from '@remix-run/cloudflare'
 import tailwindCss from '~/styles/tailwind.css'
 import indexCss from '~/styles/index.css'
@@ -29,6 +29,33 @@ export default function App() {
             </div>
           </div>
         </main>
+        <LiveReload />
+      </body>
+    </html>
+  )
+}
+
+export function ErrorBoundary({ error }: { error: unknown }) {
+  console.error(error)
+  const printableError = error instanceof Error ? error.stack : `${error}`
+  return (
+    <html lang="en" className="">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Error - Ruuvipuserrin configurator</title>
+        <Links />
+      </head>
+      <body className="h-screen">
+        <main>
+          <nav className="navbar w-full bg-zinc-100 dark:bg-zinc-900 px-2 py-4 text-4xl sm:text-5xl">Ruuvipuserrin</nav>
+          <div className="flex justify-center sm:px-8 sm:py-16">
+            <div className="mockup-code bg-primary text-primary-content">
+              <pre>{printableError}</pre>
+            </div>
+          </div>
+        </main>
+        <Scripts />
         <LiveReload />
       </body>
     </html>
