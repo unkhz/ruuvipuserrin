a685993cb641665e0aaf7fbcda3bb239119829cf
Juhani Pelli <juhani.pelli@gmail.com>
2025-04-15 22:38:51 +0300

    Update gatherer-stdin to use Bun.stdin

--- packages/gatherer-stdin/lib/listener-read.ts
+++ packages/gatherer-stdin/lib/listener-read.ts
@@ -2,11 +2,6 @@ import { readArgs } from './args.js'
 import { RuuviMeasurement } from '@ruuvipuserrin/common-data'
 import { parseLineFromRuuvitagListener } from './transform.js'
 
-import * as readline from 'node:readline'
-const standardInputStream = readline.createInterface({
-  input: process.stdin,
-})
-
 const args = readArgs()
 
 export async function processMeasurementsFromStandardInput(cb: (data: RuuviMeasurement) => void) {
@@ -14,7 +9,7 @@ export async function processMeasurementsFromStandardInput(cb: (data: RuuviMeasu
     `Reading ruuvitag-listener measurements from stdin with the name "${args.measurementName}" (configure with option --measurementName)`,
   )
 
-  standardInputStream.on('line', (line: string) => {
+  for await (const line of Bun.stdin.stream()) {
     // Only process input with sepcific ruuvitag-listener influxdb measurement name
     if (line.startsWith(args.measurementName)) {
       const measurement = parseLineFromRuuvitagListener(line)
@@ -22,5 +17,5 @@ export async function processMeasurementsFromStandardInput(cb: (data: RuuviMeasu
         cb(measurement)
       }
     }
-  })
+  }
 }
